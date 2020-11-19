3aededc8a67a002b4929fc8115873636cdcc2cf3
TanyFV <tanyfv@gmail.com>
2020-11-19 18:23:15 +0200

    init

---
+++ src/main/java/io/hexlet/typoreporter/TypoReporterApplication.java
@@ -0,0 +1,13 @@
+package io.hexlet.typoreporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypoReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypoReporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/hexlet/typoreporter/TypoReporterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hexlet.typoreporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypoReporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
