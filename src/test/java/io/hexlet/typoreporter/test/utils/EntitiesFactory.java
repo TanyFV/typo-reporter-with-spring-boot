ec4e27fd3397f587f9b11e3ae8879a22b111a4f3
TanyFV <tanyfv@gmail.com>
2020-11-25 23:07:41 +0200

    add new class EntitiesFactory

---
+++ src/test/java/io/hexlet/typoreporter/test/utils/EntitiesFactory.java
@@ -0,0 +1,29 @@
+package io.hexlet.typoreporter.test.utils;
+
+import io.hexlet.typoreporter.domain.Identifiable;
+import io.hexlet.typoreporter.domain.typo.Typo;
+
+import java.util.stream.Stream;
+
+public class EntitiesFactory {
+    public static Stream<Typo> getTypos() {
+        final var typo1 = new Typo()
+                .setId(1L)
+                .setPageUrl("PageUrl")
+                .setReporterName("ReporterName")
+                .setReporterComment("ReporterComment")
+                .setTextBeforeTypo("TextBeforeTypo")
+                .setTextTypo("TextTypo")
+                .setTextAfterTypo("TextAfterTypo");
+
+        final var typo2 = new Typo()
+                .setId(2L)
+                .setPageUrl("PageUrl")
+                .setReporterName("ReporterName");
+        return Stream.of(typo1, typo2);
+    }
+
+    public static Stream<? extends Identifiable<?>> getEntities() {
+        return getTypos();
+    }
+}
