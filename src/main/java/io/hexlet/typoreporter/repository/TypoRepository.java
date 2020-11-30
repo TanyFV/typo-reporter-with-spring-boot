816e479b06e5f1efb8b3f9d579f88eeb467c2687
TanyFV <tanyfv@gmail.com>
2020-11-30 23:36:36 +0200

    add new classes

---
+++ src/main/java/io/hexlet/typoreporter/repository/TypoRepository.java
@@ -0,0 +1,9 @@
+package io.hexlet.typoreporter.repository;
+
+import io.hexlet.typoreporter.domain.typo.Typo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TypoRepository extends JpaRepository<Typo, Long> {
+}
